57e82a2f2720e2c9a720a27811ff7ec1d2136b59
fotanus <fotanus@gmail.com>
2017-06-09 11:12:00 -0300

    add necessary environment variable test on startup

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,11 @@ require_relative 'boot'
 
 require 'rails/all'
 
+if ENV["ALPHAVANTAGE_API_KEY"].nil?
+  puts "You need to set the environment variable ALPHAVANTAGE_API_KEY to run this app."
+  exit 1
+end
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
